2f5f47125194742fdb385995015040371ad9dfbe
SHIMADA Keiki <shimada.cake@gmail.com>
2023-01-06 00:42:34 +0900

    add Windows Binary

--- pp3.c
+++ pp3.c
@@ -21,7 +21,7 @@ void comErr(char *fmt, ...);
 void flsprintf(FILE* f, char *fmt, ...);
 
 char * COM = "";
-char * PP_VERSION = "0.99";
+char * PP_VERSION3 = "0.99";
 
 #define	PROGMEM_LEN	260000
 #define	CONFIG_LEN	35
@@ -598,7 +598,7 @@ int main(int argc, char *argv[])
     unsigned char * pm_point, * cm_point;
     unsigned char tdat[200];
     parseArgs(argc,argv);
-    if (verbose>0) printf ("PP programmer for Q43/Q8x, version %s\n",PP_VERSION);
+    if (verbose>0) printf ("PP programmer for Q43/Q8x, version %s\n",PP_VERSION3);
     if (verbose>1) printf ("Opening serial port\n");
     initSerialPort();
     if (sleep_time>0)
